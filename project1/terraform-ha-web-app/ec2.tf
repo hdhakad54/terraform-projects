e2b1dff18b1569e07ef62e9157ec0b085f6a5532
hdhakad54 <hdhakad54@gmail.com>
2025-08-26 23:50:20 +0530

    9762

--- project1/terraform-ha-web-app/ec2.tf
+++ project1/terraform-ha-web-app/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
  ami = "ami-00ca32bbc84273381" # Amazon Linux 2 AMI
  instance_type = "t2.micro"
  subnet_id = aws_subnet.public.id
- key_name = "abc.pem"
+ key_name = "abc"
 
  tags = {
  Name = "web-server"
