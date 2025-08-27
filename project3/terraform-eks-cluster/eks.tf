e46e34964c131158ce7ed3b7b16d142fc459f86a
hdhakad54 <hdhakad54@gmail.com>
2025-08-27 15:43:05 +0530

    add eks nodes

--- project3/terraform-eks-cluster/eks.tf
+++ project3/terraform-eks-cluster/eks.tf
@@ -1,22 +1,20 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "19.21.0" # or your preferred version
+ source  = "terraform-aws-modules/eks/aws"
 
-  cluster_name    = "eks-cluster"
-  cluster_version = "1.25"
-  vpc_id          = aws_vpc.eks_vpc.id
-  subnet_ids      = [aws_subnet.public_subnet.id, aws_subnet.private_subnet.id]
+ cluster_name = "eks-cluster"
+ cluster_version = "1.25"
+subnet_ids =  [aws_subnet.public_subnet.id, aws_subnet.private_subnet.id]
+ vpc_id = aws_vpc.eks_vpc.id
+ eks_managed_node_groups = {
+ eks_nodes = {
+ desired_capacity = 2
+ max_capacity = 3
+ min_capacity = 1
+ instance_type = "t3.medium"
+ }
+ }
 
-  eks_managed_node_groups = {
-    eks_nodes = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
-      instance_types = ["t3.medium"]
-    }
-  }
-
-  tags = {
-    Name = "eks-cluster"
-  }
+ tags = {
+ Name = "eks-cluster"
+ }
 }
