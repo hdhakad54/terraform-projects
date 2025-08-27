f4ce8694b17a6749c8edce22ae3b65c78334f649
hdhakad54 <hdhakad54@gmail.com>
2025-08-27 20:44:03 +0530

    update k8s config

--- project3/terraform-eks-cluster/main.tf
+++ project3/terraform-eks-cluster/main.tf
@@ -1,3 +1,23 @@
 provider "aws" {
  region = "us-east-1"
 }
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.23"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.9"
+    }
+  }
+}
+
+
